f6f1d482749b46ebb65530296868a009eb426423
Pedro Henrique Maia <phmaia2018@gmail.com>
2022-10-16 17:02:20 -0300

    test: Adiciona teste unitario com HttpClientTestingModule

--- src/app/personagem/personagem.component.spec.ts
+++ src/app/personagem/personagem.component.spec.ts
@@ -1,25 +1,36 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { PersonagemComponent } from './personagem.component';
+import { HttpClient } from '@angular/common/http';
 
 describe('PersonagemComponent', () => {
   let component: PersonagemComponent;
   let fixture: ComponentFixture<PersonagemComponent>;
+  let http: HttpClient;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ PersonagemComponent ]
+      declarations: [ PersonagemComponent ],
+      imports: [ HttpClientTestingModule ]
     })
     .compileComponents();
   });
 
   beforeEach(() => {
     fixture = TestBed.createComponent(PersonagemComponent);
+    http = TestBed.inject(HttpClient);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('deve ser criado', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('deve testar o httpClient', () => {
+    spyOn(http, 'get').and.callThrough();
+    component.recuperarPersonagens();
+    expect(http.get).toHaveBeenCalled();
+    expect(http.get).toHaveBeenCalledWith('https://rickandmortyapi.com/api/character');
   });
 });
