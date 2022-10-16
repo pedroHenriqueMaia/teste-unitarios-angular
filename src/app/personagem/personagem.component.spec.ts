752df99df2a33f88e741ad669a841126c4f72254
Pedro Henrique Maia <phmaia2018@gmail.com>
2022-10-16 16:49:11 -0300

    test: Adiciona teste unitario utilizando stub

--- src/app/personagem/personagem.component.spec.ts
+++ src/app/personagem/personagem.component.spec.ts
@@ -1,14 +1,49 @@
+import { HttpClient } from '@angular/common/http';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { IPersonagemResponse } from '../models/response/IPersonagemResponse';
 
 import { PersonagemComponent } from './personagem.component';
 
 describe('PersonagemComponent', () => {
   let component: PersonagemComponent;
   let fixture: ComponentFixture<PersonagemComponent>;
+  const mock: IPersonagemResponse = {
+    info: {
+      count: 1,
+      next: '',
+      pages: 1,
+      prev: 1
+    },
+    results: [
+      {
+        id: 1,
+        name:	'',
+        status:	'',
+        species:	'',
+        type:	'',
+        gender:	'',
+        origin:	'',
+        location:	'',
+        image: '',
+        episode: [''],
+        url:	'',
+        created:	''	
+      }
+    ]
+  }
+  const httpStub = {
+    get: (params: any) => new Promise(() => mock)
+  }
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ PersonagemComponent ]
+      declarations: [ PersonagemComponent ],
+      providers: [
+        {
+          provide: HttpClient,
+          useValue: httpStub
+        }
+      ]
     })
     .compileComponents();
   });
@@ -19,7 +54,14 @@ describe('PersonagemComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('dve ser criado', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('deve testar o httpClient', () => {
+    spyOn(httpStub, 'get').and.callThrough();
+    component.recuperarPersonagens();
+    expect(httpStub.get).toHaveBeenCalled();
+    expect(httpStub.get).toHaveBeenCalledWith('https://rickandmortyapi.com/api/character');
   });
 });
